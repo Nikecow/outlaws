ced6a18fc9278fe558312d3159a8c2366bbf008a
Nikecow <nicovanduuren@gmail.com>
2015-05-12 19:00:11 +0200

    Working FPSController

--- Assets/Resources/Scripts/AudioController.cs
+++ Assets/Resources/Scripts/AudioController.cs
@@ -3,13 +3,53 @@ using System.Collections;
 
 public class AudioController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	/* Create an audio, ambient and music source */
+	private AudioSource audioSource;
+	private AudioSource ambientSource;
+	private AudioSource musicSource;
+
+	/* Create an array with the music */
+	public SoundFiles[] musicSounds, ambientSounds;
+
+	[System.Serializable]
+	public class SoundFiles {
+		private AudioClip[] ambientSounds;
+		private AudioClip[] musicSounds;
+	}
+
+
+
+	private AudioClip result;
+
+	void Start () 
+	{
+		audioSource = (AudioSource)gameObject.AddComponent <AudioSource>();
+		musicSource = (AudioSource)gameObject.AddComponent <AudioSource>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	internal void PlayMusic (string music)
+	{
+		//SoundFiles musicSounds = new SoundFiles();
+		//musicSounds.musicSounds
+		float soundVolume = 0;
+		
+		switch (music) 
+		{
+		case "menu":
+//			result = musicSounds.musicSounds[0];
+			soundVolume = 0.2f; 
+			break;
+		}
+//		musicSource.PlayOneShot (result, soundVolume);
+	}
+	
+	internal void StopMusic () 
+	{
+		musicSource = GameObject.Find ("AudioController(Clone)").GetComponent<AudioSource> ();
+		musicSource.Stop ();
 	}
 }
